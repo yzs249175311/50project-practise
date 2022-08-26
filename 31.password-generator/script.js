725b1dc7e4b876159f5a69928ddab7b70f80c4b3
yuanzishuai <yuanzishuai@qq.com>
2022-08-26 14:09:09 +0800

    add 31

---
+++ 31.password-generator/script.js
@@ -0,0 +1,41 @@
+let show = document.querySelector("#show")
+let length = document.querySelector("#length")
+let upper = document.querySelector("#upper")
+let lower = document.querySelector("#lower")
+let numbers = document.querySelector("#numbers")
+let symbols = document.querySelector("#symbols")
+let generate = document.querySelector("#generate")
+let clipboard = document.querySelector("#clipboard")
+
+let passDic = {
+	lower: "qwertyuiopasdfghjklzxcvbnm",
+	upper: "QWERTYUIOPASDFGHJKLZXCVBNM",
+	numbers: "1234567890",
+	symbols: `\`~!@#$%^&*()_+-={}[]:";'<>?,./|`
+}
+
+
+function generatePassword () {
+	let arr = [upper, lower, numbers, symbols].reduce((oldVal, newVal) => {
+		if (newVal.checked) {
+			return oldVal + passDic[newVal.value]
+		}
+		return oldVal
+	}, "")
+
+	let res = ""
+	for (let i = parseInt(length.value); i > 0; i--) {
+		res = res + arr.at(Math.floor(Math.random() * arr.length))
+	}
+	show.innerText = res
+}
+
+generate.addEventListener("click", generatePassword)
+
+clipboard.addEventListener("click",async function(){
+	if(show.innerText){
+		await window.navigator.clipboard.writeText(show.innerText)
+		alert("已经复制了密码: "+show.innerText)
+	}
+})
+
--- script.js
+++ script.js
@@ -1,50 +1,51 @@
 let main = document.querySelector("main");
 let toTop = document.querySelector("#top");
 let projects = [
-  "01.expanding-cards",
-  "02.progress-steps",
-  "03.rotating-navigation-animation",
-  "04.hidden-search-widget",
-  "05.blurry-loading",
-  "06.scroll-animation",
-  "07.split-landing-page",
-  "08.form-input-wave",
-  "09.sound-board",
-  "10.dad-jokes",
-  "11.event-keycodes",
-  "12.faq-collapse",
-  "13.random-choice-picker",
-  "14.animated-navigation",
-  "15.incrementing-counter",
-  "16.drink-water",
-  "17.movie-app",
-  "18.background-slider",
-  "19.theme-clock",
-  "20.button-ripple-effect",
-  "21.drag-n-drop",
-  "22.drawing-app",
-  "23.kinetic-loader",
-  "24.content-placeholder",
-  "25.sticky-navbar",
-  "26.double-vertical-slider",
-  "27.toast-notification",
-  "28.github-profiles",
-  "29.double-click-heart",
-  "30.auto-text-effect",
-  "virtual scroller",
-  "virtual-scroller-v2",
+	"01.expanding-cards",
+	"02.progress-steps",
+	"03.rotating-navigation-animation",
+	"04.hidden-search-widget",
+	"05.blurry-loading",
+	"06.scroll-animation",
+	"07.split-landing-page",
+	"08.form-input-wave",
+	"09.sound-board",
+	"10.dad-jokes",
+	"11.event-keycodes",
+	"12.faq-collapse",
+	"13.random-choice-picker",
+	"14.animated-navigation",
+	"15.incrementing-counter",
+	"16.drink-water",
+	"17.movie-app",
+	"18.background-slider",
+	"19.theme-clock",
+	"20.button-ripple-effect",
+	"21.drag-n-drop",
+	"22.drawing-app",
+	"23.kinetic-loader",
+	"24.content-placeholder",
+	"25.sticky-navbar",
+	"26.double-vertical-slider",
+	"27.toast-notification",
+	"28.github-profiles",
+	"29.double-click-heart",
+	"30.auto-text-effect",
+	"31.password-generator",
+	"virtual scroller",
+	"virtual-scroller-v2",
 ];
 
 projects.forEach(function (name) {
-  main.innerHTML += `<a href="${name}/index.html" target="_blank">${name}</a>`;
+	main.innerHTML += `<a href="${name}/index.html" target="_blank">${name}</a>`;
 });
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY > 200) {
-    toTop.style.visibility = "visible";
-    toTop.style.opacity = "1";
-  } else {
-    toTop.style.visibility = "hidden";
-    toTop.style.opacity = "0";
-  }
+	if (window.scrollY > 200) {
+		toTop.style.visibility = "visible";
+		toTop.style.opacity = "1";
+	} else {
+		toTop.style.visibility = "hidden";
+		toTop.style.opacity = "0";
+	}
 });
