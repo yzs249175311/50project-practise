50593a4b017c789d4387b130b239ef802a948f9e
yuanzishuai <yuanzishuai@qq.com>
2022-10-18 18:43:31 +0800

    fix 41

--- 41.verify-account-ui/script.ts
+++ 41.verify-account-ui/script.ts
@@ -1,13 +1,14 @@
-let boxs: HTMLInputElement[] = document.querySelectorAll(".box")
+let boxs: NodeListOf<HTMLInputElement> = document.querySelectorAll(".box")
 
-boxs.forEach((box, index) => {
+boxs.forEach((box:HTMLInputElement, index:number) => {
 
 	box.addEventListener("keydown", (e: KeyboardEvent) => {
 		if (e.key == "Backspace") {
-			setTimeout(() => { boxs[index == 0 ? 0 : index - 1].focus() }, 10)
+			setTimeout(() => { (boxs[index == 0 ? 0 : index - 1]).focus() }, 10)
 		} else if (/\d/.test(e.key)) {
 			setTimeout(() => { boxs[index == 5 ? index : index + 1].focus(), 10 })
+		} else {
+			setTimeout(() => { boxs[index].value = "" })
 		}
-
-	}
+	})
 })
