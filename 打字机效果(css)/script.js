0f0b1a0c15d112a1b498c5c46d4168c85cd72e85
yuanzishuai <yuanzishuai@qq.com>
2024-01-04 18:42:43 +0800

    优化打字机效果

--- 打字机效果(css)/script.js
+++ 打字机效果(css)/script.js
@@ -4,18 +4,22 @@ let styleElement = document.createElement("style");
 document.head.appendChild(styleElement);
 
 styleElement.sheet.insertRule(
-  `
-    #content::before{
-         animation: typewriter ${content.innerText.length / 2}s steps(${ content.innerText.length }) infinite,
-         flashing 0.3s ease forwards infinite; 
-        }
     `
+    #content::before{
+         animation: typewriter ${content.innerText.length / 2}s steps(${
+             content.innerText.length
+         }) alternate infinite,
+         flashing 0.3s ease forwards infinite;
+        }
+    `,
 );
 
 styleElement.sheet.insertRule(
-  `
+    `
     #content::after {
-         animation: typewriter ${content.innerText.length / 2}s steps(${ content.innerText.length }) infinite;
+         animation: typewriter ${content.innerText.length / 2}s steps(${
+             content.innerText.length
+         }) alternate infinite;
     }
-`
+`,
 );
